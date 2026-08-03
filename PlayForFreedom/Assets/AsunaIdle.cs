057d046fba9580d997839608c371d3d422baa5e3
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2026-08-03 13:32:35 +0100

    added asuna animations and some prefabs

---
+++ PlayForFreedom/Assets/AsunaIdle.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class AsunaIdle : StateMachineBehaviour
+{
+    [Range(1, 50)]
+    [SerializeField] int maxStates = 1;
+
+
+    private static readonly int IdleStyleHash = Animator.StringToHash("IdleStyle");
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        SetIdleStyle(animator);
+    }
+
+    private void SetIdleStyle(Animator animator = null)
+    {
+        if (maxStates <= 1) return;
+        if (animator) animator.SetInteger(IdleStyleHash, Random.Range(0, maxStates));
+
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
